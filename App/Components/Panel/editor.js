d7a9c23faf7576848c877c5ed7fffeb3ddba2719
markentingh <entingh@gmail.com>
2015-09-09 23:20:24 -0700

    update logo

--- App/Components/Panel/editor.js
+++ App/Components/Panel/editor.js
@@ -16,6 +16,9 @@
 
     var options = { id: c.id.substr(1), aboveId: aboveId, duplicate: 0 };
     //first, send an AJAX request to save page changes
+
+    console.log(options);
+
     S.editor.save.click(function () {
         //then duplicate component
         S.ajax.post('/websilk/Components/Panel/DuplicateCell', options, S.ajax.callback.inject);
